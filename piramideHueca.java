c3b3eebeb6f9ffdbbec8d56a9e5a8740fc7a7ea7
Jesushs4 <lagt5799@gmail.com>
2023-02-22 09:39:39 +0100

    Revert "added new character change feature"
    This reverts commit 3ad334c602fe0dda45bb019ab8835fcfa1319d47.

--- piramideHueca.java
+++ piramideHueca.java
@@ -1,28 +1,26 @@
 import java.util.Scanner;
-public class piramideHueca {
+public class Ejercicio20 {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         int cont2 = 0;
         System.out.print("Introduzca la altura de la pirámide: ");
         int altura = sc.nextInt();
-        System.out.print("Introduzca el carácter: ");
-        char cara = sc.next().charAt(0);
         for (int cont1=1; cont1<=altura; cont1++) {
             System.out.print(" ");
-        } System.out.println(cara);
+        } System.out.println("*");
         for (int cont1=2; cont1<=altura*2-3; cont1+=2) {
             for (cont2=cont1; cont2<=altura*2-2; cont2+=2) {
                 System.out.print(" ");
             }
-            System.out.print(cara + " ");
+            System.out.print("* ");
             for (cont2=cont1; cont2-2>=1; cont2--) {
                 System.out.print(" ");
-            } System.out.print(cara);
+            } System.out.print("*");
             System.out.println(""); 
         }
         System.out.print(" ");
         for (int cont1=1; cont1<=altura*2-1; cont1++) {
-            System.out.print(cara);
+            System.out.print("*");
         }
     }
-}
+ }
